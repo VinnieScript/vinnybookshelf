6fedb75c8299b846f99e601a536ac21e501dabcf
davincydacode <hademylola@gmail.com>
2019-07-01 21:20:10 +0100

    'saved'

--- config/app.php
+++ config/app.php
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY','SomeRandomString'),
+    'key' => env('APP_KEY','aTPnl99qSgNpByGlqRstujwbj6N6j2DyWe3ar0ZVqtc='),
 
     'cipher' => 'AES-128-CBC',
 
